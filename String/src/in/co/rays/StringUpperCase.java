f508e18f5cb907fd0b031cc701eff5bb95910069
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-19 12:03:05 +0530

    Basics

---
+++ String/src/in/co/rays/StringUpperCase.java
@@ -0,0 +1,11 @@
+package in.co.rays;
+import java.lang.String;
+
+public class StringUpperCase {
+	public static void main(String[] args) {
+		String name = "Vijay Dinanath Cauhan";
+		
+		System.out.println("Uppercase - " + name.toUpperCase());
+	}
+
+}
