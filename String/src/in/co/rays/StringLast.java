0129933c5a78db8bbe1a03e7de413c9ed10256ab
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-19 11:53:07 +0530

    Basics

---
+++ String/src/in/co/rays/StringLast.java
@@ -0,0 +1,11 @@
+package in.co.rays;
+import java.lang.String;
+
+public class StringLast {
+	public static void main(String[] args) {
+		String name = "Vijay Dinanath Cauhan";
+		
+		System.out.println("Last 'i' position - " + name.lastIndexOf('i'));
+	}
+
+}
