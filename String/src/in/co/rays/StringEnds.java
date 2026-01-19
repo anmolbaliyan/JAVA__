641dfc7c89479bb78620dd9e2239a51c20c0cef0
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-19 12:13:13 +0530

    Basics

---
+++ String/src/in/co/rays/StringEnds.java
@@ -0,0 +1,11 @@
+package in.co.rays;
+import java.lang.String;
+
+public class StringEnds {
+	public static void main(String[] args) {
+		String name = "Vijay Dinanath Cauhan";
+		
+		System.out.println("ends with 'han' - " + name.endsWith("han") );
+	}
+
+}
---
+++ String/src/in/co/rays/SubString.java
@@ -0,0 +1,11 @@
+package in.co.rays;
+import java.lang.String;
+
+public class SubString {
+	public static void main(String[] args) {
+		String name = "Vijay Dinanath Cauhan";
+		
+		System.out.println("Substring from index 6 - " + name.substring(15));
+	}
+
+}
