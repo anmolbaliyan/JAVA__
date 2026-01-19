852154b3f2408296886e24d5548e95811f5e53e3
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-19 11:29:20 +0530

    Basics

---
+++ String/src/in/co/rays/StringLength.java
@@ -0,0 +1,12 @@
+package in.co.rays;
+import java.lang.String;
+
+public class StringLength {
+	public static void main(String[] args) {
+		String name = "Vijay Dinanath Cauhan";
+		
+		System.out.println("Strinh length - " + name.length());
+		
+	}
+
+}
