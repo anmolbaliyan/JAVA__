26498e777f5fdb268c1cc309d81d98cc2f29be69
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-21 00:26:11 +0530

    Basics

---
+++ Array/src/in/co/rays/FindPositionOfAIntInArray.java
@@ -0,0 +1,19 @@
+package in.co.rays;
+
+public class FindPositionOfAIntInArray {
+	public static void main(String[] args) {
+		int[] arr = { 18, 17, 16, 14, 14, 5, 6, 7, 8 };
+		int num = 14;
+
+		int temp = -1;
+		for (int i = 0; i < arr.length; i++) {
+			if (num == arr[i]) {
+				temp = i;
+				System.out.println(num + " = " + temp);
+			}
+		}
+
+		System.out.println(temp == -1 ? temp : "");
+	}
+
+}
