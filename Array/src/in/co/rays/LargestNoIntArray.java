8d57cf8b1390bd40d6fcd0be8093f753e3e69d95
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-21 00:28:04 +0530

    Basics

---
+++ Array/src/in/co/rays/LargestNoIntArray.java
@@ -0,0 +1,18 @@
+package in.co.rays;
+
+public class LargestNoIntArray {
+	public static void main(String[] args) {
+		int[] intArray = { 5, 10, 15, 6, 20, 14 };
+
+		int largestNo = intArray[0];
+
+		for (int i = 0; i < intArray.length; i++) {
+			if (intArray[i] > largestNo) {
+				largestNo = intArray[i];
+			}
+		}
+
+		System.out.println("largest no is: " + largestNo);
+	}
+
+}
