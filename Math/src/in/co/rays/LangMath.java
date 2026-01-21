fafc9fd04c524db0d5bafcd686ea0ee0e3359424
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-21 14:30:47 +0530

    Basics

--- Math/src/in/co/rays/LangMath.java
+++ Math/src/in/co/rays/LangMath.java
@@ -1,5 +1,16 @@
 package in.co.rays;
+import java.lang.Math;
 
 public class LangMath {
+	public static void main(String[] args) {
+		
+		System.out.println("Mathematical Functions");
+		System.out.println("Max 2,5 - " + Math.max(2, 5));
+		System.out.println("Min 10,115 - " + Math.min(10, 115));
+		System.out.println("Absolute of 4.88 - " + Math.abs(4.88));
+		System.out.println("Exp 10 - " + Math.exp(10));
+		System.out.println("Random Number - " + Math.random());
+		System.out.println("Square Root - " + Math.sqrt(5));
+	}
 
 }
