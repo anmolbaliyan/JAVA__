40107086932a71a283553578f1d7638b8a9f2a36
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-22 13:13:52 +0530

    Basics

--- OOP/src/com/rays/encapsulation/Person.java
+++ OOP/src/com/rays/encapsulation/Person.java
@@ -1,5 +1,36 @@
 package com.rays.encapsulation;
 
+import java.time.LocalDate;
+
 public class Person {
+	
+	private String Name;
+	private LocalDate dob;
+	private String Address;
+	
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+	
+	public void setdob(LocalDate dob) {
+		this.dob = dob;
+	}
+	
+	public void setAddress(String Address) {
+		this.Address = Address;
+	}
+	
+	public String getName() {
+		return this.Name;
+	}
+	
+	public LocalDate getdob() {
+		return this.dob;
+	}
+	
+	public String getAddress() {
+		return this.Address;
+	}
+	
 
 }
---
+++ OOP/src/com/rays/encapsulation/TestPerson.java
@@ -0,0 +1,30 @@
+package com.rays.encapsulation;
+
+import java.time.LocalDate;
+
+public class TestPerson {
+	public static void main(String[] args) {
+		
+		Person p1 = new Person();
+		Person p2 = new Person();
+		
+		p1.setName("ANMOL");
+		p1.setdob(LocalDate.of(2003, 03, 9));
+		p1.setAddress("Mahalaxmi Nagar");
+		
+		p2.setName("ARUN");
+		p2.setdob(LocalDate.of(1974, 06, 20));
+ 		p2.setAddress("Mahalaxmi Nagar");
+ 		
+ 		System.out.println(p1.getName());
+ 		System.out.println(p1.getdob());
+ 		System.out.println(p1.getAddress());
+ 		
+ 		System.out.println("---------------------------");
+ 		
+ 		System.out.println(p2.getName());
+ 		System.out.println(p2.getdob());
+ 		System.out.println(p2.getAddress());
+	}
+
+}
