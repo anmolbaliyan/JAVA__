a3f03a21c9c50ae77c98324d076fc3a8e10cbc75
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-22 14:00:38 +0530

    Basics

--- OOP/src/com/rays/encapsulation/Automobile.java
+++ OOP/src/com/rays/encapsulation/Automobile.java
@@ -1,5 +1,35 @@
 package com.rays.encapsulation;
 
 public class Automobile {
+	
+	private String color;
+	private int speed;
+	private String make;
+	
+	public void setcolor(String color) {
+		this.color = color;
+	}
+	
+	public void setspeed(int speed) {
+		this.speed = speed; 
+	}
+	
+	public void setmake(String make) {
+		this.make = make;
+	}
+	
+	public String getcolor() {
+		return this.color;
+	}
+	
+	public int getspeed() {
+		return this.speed;
+	}
+	
+	public String getmake() {
+		return this.make;
+	}
 
 }
+
+
--- OOP/src/com/rays/encapsulation/TestAutomobile.java
+++ OOP/src/com/rays/encapsulation/TestAutomobile.java
@@ -1,5 +1,39 @@
 package com.rays.encapsulation;
 
 public class TestAutomobile {
+	public static void main(String[] args) {
+		
+		Automobile b1 = new Automobile();
+		Automobile b2 = new Automobile();
+		Automobile b3 = new Automobile();
+		
+		b1.setcolor("RED");
+		b1.setspeed(320);
+		b1.setmake("DUCATI");
+		
+		b2.setcolor("DEEP BLUE");
+		b2.setspeed(293);
+		b2.setmake("YAMAHA");
+		
+		b3.setcolor("BLACK");
+		b3.setspeed(230);
+		b3.setmake("APRILIA");
+		
+		System.out.println(b1.getcolor());
+		System.out.println(b1.getspeed());
+		System.out.println(b1.getmake());
+		
+		System.out.println("----------------------------");
+		
+		System.out.println(b2.getcolor());
+		System.out.println(b2.getspeed());
+		System.out.println(b2.getmake());
+		
+		System.out.println("-----------------------------");
+		
+		System.out.println(b3.getcolor());
+		System.out.println(b3.getspeed());
+		System.out.println(b3.getmake());
+	}
 
 }
