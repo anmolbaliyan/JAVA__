75db997cedfc193f4798214119828b3ad97b1959
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-22 13:49:53 +0530

    Basics

---
+++ OPERATORS/src/com/rays/operstors/PrePost.java
@@ -0,0 +1,10 @@
+package com.rays.operstors;
+
+public class PrePost {
+	public static void main(String[] args) {
+		 int i = 345;
+		 
+		 System.out.println(++i + ++i + i-- + ++i - -- i + ++i );
+	}
+
+}
