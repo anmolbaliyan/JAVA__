0c9da027fc9c41ea381deddc5427ae1b28a76200
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-23 15:37:16 +0530

    Basics

---
+++ INHERITANCE/src/com/rays/inheritance/Rectangle.java
@@ -0,0 +1,28 @@
+package com.rays.inheritance;
+
+public class Rectangle extends Shape {
+	private int lenght;
+	private int width;
+
+	public int getLenght() {
+		return lenght;
+	}
+
+	public void setLenght(int lenght) {
+		this.lenght = lenght;
+	}
+
+	public int getWidht() {
+		return width;
+	}
+
+	public void setWidht(int widht) {
+		this.width = widht;
+	}
+
+	public void area() {
+		System.out.println("area of rectangle: " + (lenght * width));
+	}
+
+
+}
---
+++ INHERITANCE/src/com/rays/inheritance/Shape.java
@@ -0,0 +1,24 @@
+package com.rays.inheritance;
+
+public class Shape {
+	
+	protected String color;
+	protected int borderWidth;
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public void setBorderWidth(int borderWidth) {
+		this.borderWidth = borderWidth;
+	}
+	
+	public int getBorderWidth() {
+		return borderWidth;
+	}
+
+}
