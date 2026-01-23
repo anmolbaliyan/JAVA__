153983df398611f29ffaaa92e285329e803f5645
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-23 13:40:40 +0530

    Basics

--- OOP/src/com/rays/encapsulation/Automobile.java
+++ OOP/src/com/rays/encapsulation/Automobile.java
@@ -5,6 +5,7 @@ public class Automobile {
 	private String color;
 	private int speed;
 	private String make;
+	public final static int NO_OF_GEARS = 6;
 	
 	public void setcolor(String color) {
 		this.color = color;
@@ -28,6 +29,35 @@ public class Automobile {
 	
 	public String getmake() {
 		return this.make;
+	}
+	
+	public void brake() {
+		if (speed == 0) {
+			System.out.println("the bike has already stopped");
+		}
+		else {
+			speed = speed - 17;
+		}
+	}
+	
+	public void accelerator() {
+		if (speed == 350 || speed > 350) {
+			System.out.println("the speed is high apply brake");
+		}
+		else {
+			speed = speed + 25;
+		}
+	}
+	
+	public void changeGear(int gear) {
+		if(gear > NO_OF_GEARS) {
+			System.out.println("no of gear enterd is invalid");
+		}
+		if (gear == 5) {
+			System.out.println("gear switched 5");
+			speed = speed +80;
+			System.out.println("current speed is: " + speed);
+		}
 	}
 
 }
--- OOP/src/com/rays/encapsulation/TestAutomobile.java
+++ OOP/src/com/rays/encapsulation/TestAutomobile.java
@@ -10,14 +10,23 @@ public class TestAutomobile {
 		b1.setcolor("RED");
 		b1.setspeed(320);
 		b1.setmake("DUCATI");
+		b1.brake();
+		b1.accelerator();
+		b1.changeGear(5);
 		
 		b2.setcolor("DEEP BLUE");
 		b2.setspeed(293);
 		b2.setmake("YAMAHA");
+		b2.brake();
+		b2.accelerator();
+		b2.changeGear(5);
 		
 		b3.setcolor("BLACK");
 		b3.setspeed(230);
 		b3.setmake("APRILIA");
+		b3.brake();
+		b3.accelerator();
+		b3.changeGear(5);
 		
 		System.out.println(b1.getcolor());
 		System.out.println(b1.getspeed());
