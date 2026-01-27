d631615388390c561a524864cc109e29f25eafe1
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-27 13:39:20 +0530

    Basics

---
+++ OOP/src/com/rays/polymorphism/TestPolymorphishByArray.java
@@ -0,0 +1,31 @@
+package com.rays.polymorphism;
+
+public class TestPolymorphishByArray {
+	public static void main(String[] args) {
+		Shape[] s = new Shape[3];
+		//System.out.println("length of shape array:" + s.length);
+		
+		s[0] = new Circle();
+		s[1] = new Rectangle();
+		s[2] = new Triangle();
+		
+		Circle c = (Circle) s[0];
+		c.setRadius(45);
+		
+		Rectangle r = (Rectangle) s[1];
+		r.setLength(76);
+		r.setwidth(54);
+		
+		Triangle t = (Triangle) s[2];
+		t.setBase(45);
+		t.setHeight(67);
+		
+		for (int i = 0;i>s.length;i++) {
+			
+			s[i].area();
+		}
+		
+	}
+	
+
+}
