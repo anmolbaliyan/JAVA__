ebe72b0d31a95a51df2d7d06564721997e7987fd
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-27 13:01:51 +0530

    Basics

--- OOP/src/com/rays/polymorphism/TestBank.java
+++ OOP/src/com/rays/polymorphism/TestBank.java
@@ -4,7 +4,7 @@ public class TestBank {
 	
 	public static void main(String[] args) {
 
-		Banks b1 = new AxisBank();
+/*		Banks b1 = new AxisBank();
 
 		System.out.println(b1.getName());
 		System.out.println(b1.InterestRate());
@@ -22,7 +22,25 @@ public class TestBank {
 
 		System.out.println(b3.getName());
 		System.out.println(b3.InterestRate());
-
+*/
+		
+		Banks[] banks = new Banks[3];
+		
+		banks[0] = new AxisBank();
+		banks[1] = new HdfcBank();
+		banks[2] = new IciciBank();
+		
+		loanEnquiry(banks);
+		
+	}
+	
+	static void loanEnquiry(Banks[] banks) {
+		for (Banks b : banks) {
+			String name = b.getName();
+			double rate = b.InterestRate();
+			
+			System.out.println(name + "=" + rate);
+		}
 	}
 
 
