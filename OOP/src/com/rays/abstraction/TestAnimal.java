96ac787f73bab85e646f1c78860b9467eb5a1556
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-01-30 00:04:07 +0530

    Basics

---
+++ OOP/src/com/rays/abstraction/TestAnimal.java
@@ -0,0 +1,20 @@
+package com.rays.abstraction;
+
+public class TestAnimal {
+	public static void main(String[] args) {
+//		Animal a = new Animal();
+
+		Animal a1 = new Cat();
+
+		a1.say();
+
+		Animal a2 = new Dog();
+
+		a2.say();
+		
+		Animal a3 = new Tiger();
+		
+		a3.say();
+	}
+
+}
