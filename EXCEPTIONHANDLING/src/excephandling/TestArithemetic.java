7b994d6f57990bff48b9f05eef1835f9f98491ee
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-02-01 21:47:08 +0530

    Basics

---
+++ EXCEPTIONHANDLING/src/excephandling/TestArithemetic.java
@@ -0,0 +1,16 @@
+package excephandling;
+
+public class TestArithemetic {
+	
+public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 0;
+		int c = a / b;
+		
+		System.out.println(c);
+		System.out.println("programe is finish");
+		
+	}
+
+}
