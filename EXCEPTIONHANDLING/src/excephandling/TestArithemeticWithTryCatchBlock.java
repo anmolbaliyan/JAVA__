d4c6cedc2955b874a9c9c288ca6e8741f0ad28af
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-02-01 21:48:42 +0530

    Basics

---
+++ EXCEPTIONHANDLING/src/excephandling/TestArithemeticWithTryCatchBlock.java
@@ -0,0 +1,21 @@
+package excephandling;
+
+public class TestArithemeticWithTryCatchBlock {
+	
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 0;
+		try {
+			int c = a / b; // ArithmeticException
+			System.out.println("c: " + c);
+		} catch (ArithmeticException e) {
+			System.out.println("exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		System.out.println("programe is finish");
+
+	}
+
+}
