6ff7d563f55349f784413502b83c6f0b40ba763f
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-02-02 12:42:24 +0530

    Basics

---
+++ EXCEPTIONHANDLING/src/excephandling/CheckedPropogation.java
@@ -0,0 +1,30 @@
+package excephandling;
+
+public class CheckedPropogation {
+	
+	public static void main(String[] args) {
+		
+		try {
+			dad();
+		}catch(Exception e){
+			System.out.println("exception: " + e.getMessage());
+		}
+		
+	}
+	
+	public static void dad() throws Exception {
+		mom();
+	}
+	
+	public static void mom() throws Exception {
+		
+		son();
+	}
+	
+	public static void son() throws Exception{
+		
+		throw new Exception("make a mistake");
+		
+	}
+
+}
