a5692b0d8483a50e20d1929ba966a49d7891f658
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-02-03 13:26:26 +0530

    Basics

---
+++ COLLECTION/src/com/rays/collections/list/TestVector.java
@@ -0,0 +1,19 @@
+package com.rays.collections.list;
+
+import java.util.Vector;
+
+public class TestVector {
+
+	public static void main(String[] args) {
+
+		Vector v = new Vector();
+
+		v.addElement("Ram");
+		v.addElement(45);
+
+		System.out.println(v.elementAt(1));
+		System.out.println(v.get(1));
+
+	}
+
+}
