eef287521d984b863d1ee8a8d25c7717293e9aad
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-02-10 14:08:26 +0530

    Basics

---
+++ IO_Stream/src/com/rays/iostreams/ReadTextFile.java
@@ -0,0 +1,23 @@
+package com.rays.iostreams;
+
+import java.io.FileReader;
+
+public class ReadTextFile {
+	
+	public static void main(String[] args) throws Exception{
+		
+		FileReader file = new FileReader("/Users/anmolkumarbaliyan/Desktop/IO/Helloo.txt");
+		
+		int i = file.read();
+		
+		while( i != -1 ) {
+			System.out.println((char) i + " = " + i );
+			i = file.read();
+		}
+		
+		file.close();
+		
+		
+	}
+
+}
