b776dff721269867b159e5fa4aa01abf90d8af22
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-02-11 18:49:59 +0530

    Basics

---
+++ IO_Stream/src/com/rays/iostreams/Serialization/TestEmployeeExternalizable.java
@@ -0,0 +1,30 @@
+package com.rays.iostreams.Serialization;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class TestEmployeeExternalizable {
+
+	public static void main(String[] args) throws Exception {
+
+		Employee e = new Employee(1, "Ram");
+
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("/Users/anmolkumarbaliyan/Desktop/IO/Employee.txt"));
+
+		out.writeObject(e);
+
+		out.close();
+
+		System.out.println("object serialized successfully");
+
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream("/Users/anmolkumarbaliyan/Desktop/IO/Employee.txt"));
+
+		System.out.println(in.readObject());
+
+		in.close();
+
+	}
+
+}
