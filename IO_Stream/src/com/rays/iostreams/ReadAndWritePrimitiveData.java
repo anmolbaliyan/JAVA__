1312a0434ee880bdb9a48a68f681122b7b14d491
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-02-11 16:00:55 +0530

    Basics

---
+++ IO_Stream/src/com/rays/iostreams/ReadAndWritePrimitiveData.java
@@ -0,0 +1,35 @@
+package com.rays.iostreams;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ReadAndWritePrimitiveData {
+
+	public static void main(String[] args) throws IOException {
+
+		DataOutputStream out = new DataOutputStream(new FileOutputStream("/Users/anmolkumarbaliyan/Desktop/IO/Primitive.txt"));
+
+		out.writeInt(1);
+		out.writeBoolean(false);
+		out.writeDouble(45.25);
+		out.writeChar('A');
+
+		out.close();
+
+		System.out.println("data added......");
+
+		DataInputStream in = new DataInputStream(new FileInputStream("/Users/anmolkumarbaliyan/Desktop/IO/Primitive.txt"));
+
+		System.out.println(in.readInt());
+		System.out.println(in.readBoolean());
+		System.out.println(in.readDouble());
+		System.out.println(in.readChar());
+
+		in.close();
+
+	}
+
+}
