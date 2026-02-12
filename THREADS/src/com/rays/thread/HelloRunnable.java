a22a16430cdac23d01d2fdd4571d4b2224459228
anmolbaliyan <anmolbaliyan09@gmail.com>
2026-02-12 12:57:03 +0530

    Basics

--- THREADS/src/com/rays/thread/HelloRunnable.java
+++ THREADS/src/com/rays/thread/HelloRunnable.java
@@ -1,5 +1,23 @@
 package com.rays.thread;
 
-public class HelloRunnable {
+public class HelloRunnable implements Runnable {
+
+	private String name;
+
+	public HelloRunnable(String name) {
+
+		this.name = name;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i + " = " + name);
+		}
+
+	}
 
 }
--- THREADS/src/com/rays/thread/TestHelloThread.java
+++ THREADS/src/com/rays/thread/TestHelloThread.java
@@ -1,5 +1,19 @@
 package com.rays.thread;
 
-public class TestHelloThread {
+public class TestHelloThread  {
+	
+	public static void main(String[] args) {
+		 
+		HelloThread t1 = new HelloThread("Ram");
+		HelloThread t2 = new HelloThread("Shyam");
+		
+		t1.start();
+		t2.start();
+		
+		for (int i =1 ; i <=5 ; i++) {
+			System.out.println("hello World");
+		}
+	}
+	
 
 }
